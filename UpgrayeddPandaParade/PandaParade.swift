00602af516458b5547a2d3300f195692f7cbca5b
FullMetalFist <m.vilabrera@gmail.com>
2025-06-25 15:15:58 -0400

    initial commit

---
+++ UpgrayeddPandaParade/PandaParade.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UpgrayeddPandaParade
+//
+//  Created by Michael Vilabrera on 6/25/25.
+//
+
+import SwiftUI
+
+struct PandaParade: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    PandaParade()
+}
---
+++ UpgrayeddPandaParade/UpgrayeddPandaParadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpgrayeddPandaParadeApp.swift
+//  UpgrayeddPandaParade
+//
+//  Created by Michael Vilabrera on 6/25/25.
+//
+
+import SwiftUI
+
+@main
+struct UpgrayeddPandaParadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PandaParade()
+        }
+    }
+}
